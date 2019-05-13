bb540d15ef75846454c92074f34e7fe4e4ac9b04
voluble-bug-0219 <montensia1@outlook.com>
2019-05-13 01:37:47 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -14,7 +14,7 @@ def loop_iterator(number_of_times)
     phrase = "Welcome to Flatiron School's Web Development Course!"
       puts phrase
       counter += 1
-      break if counter < number_of_times
+      break if counter == 4
   end
 end
 
